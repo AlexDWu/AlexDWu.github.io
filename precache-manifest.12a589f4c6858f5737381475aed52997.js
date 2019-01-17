6e26584b04f6b14f951f692491a5168fdd0d894f
Alex Wu <hehahohee@gmail.com>
2019-01-17 18:15:32 +0800

    Updates

---
+++ precache-manifest.12a589f4c6858f5737381475aed52997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/decision-maker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "756e49549f44cd90ee6d",
+    "url": "/decision-maker/static/js/runtime~main.756e4954.js"
+  },
+  {
+    "revision": "b08b19421a1efb46a555",
+    "url": "/decision-maker/static/js/main.b08b1942.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/decision-maker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b08b19421a1efb46a555",
+    "url": "/decision-maker/static/css/main.5e80439e.chunk.css"
+  },
+  {
+    "revision": "eda03d571011353e710ac6a969fcaf6e",
+    "url": "/decision-maker/index.html"
+  }
+];
